ac4bbde1f685ddd39fa21477f50fcb617f6bc369
Trần Đức Minh <36166484+tranducminh@users.noreply.github.com>
2021-05-30 03:02:43 +0700

    add env config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
-const localeSubpaths = {}
-
 module.exports = {
+  env: {
+    DOMAIN_NAME: process.env.DOMAIN_NAME,
+  },
   async redirects() {
     return [
       {
         source: '/',
         destination: '/employee',
+        permanent: true,
+      },
+      {
+        source: '/admin',
+        destination: '/admin/buildings',
+        permanent: true,
+      },
+      {
+        source: '/repairman',
+        destination: '/repairman/tasks',
         permanent: true,
       },
     ]
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: `${process.env.DOMAIN_NAME}/api`,
 })
 
 instance.interceptors.request.use(
